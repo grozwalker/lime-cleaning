40b26afbd6b35e0b8aed8450bafeff4c45319f2d
Andrey Groza <grozwalker@gmail.com>
2017-10-03 21:30:09 +0300

    Remove tags in analytics partials

--- resources/views/frontend/partials/analitycs.blade.php
+++ resources/views/frontend/partials/analitycs.blade.php
@@ -33,7 +33,6 @@ if (App::environment('production')){
 <noscript><div><img src="https://mc.yandex.ru/watch/45854880" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
-HEAD
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -41,7 +40,7 @@ HEAD
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-T8WP7RV');</script>
 <!-- End Google Tag Manager -->
-BODY
+
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8WP7RV"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
